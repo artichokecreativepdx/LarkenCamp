ed25c99d2be6820eb011611164a89aa08c88badd
megan <me.maldy@gmail.com>
2023-02-04 21:30:03 -0800

    Update App.tsx
    app.tsx file has the api stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import logo from './larkenCamp.svg';
-import axios from 'axios';
+// import axios from 'axios';
 
-export type Parks = {
-  CampsiteID: 'string';
-  FacilityID: 'string';
-  CampsiteName: 'string';
-  CampsiteType: 'string';
-  TypeOfUse: 'string';
-  Loop: 'string';
-  CampsiteAccessible: true;
-  CampsiteLongitude: -118.0186111;
-  CampsiteLatitude: 44.6969444;
-};
+// export type Parks = {
+//   CampsiteID: 'string';
+//   FacilityID: 'string';
+//   CampsiteName: 'string';
+//   CampsiteType: 'string';
+//   TypeOfUse: 'string';
+//   Loop: 'string';
+//   CampsiteAccessible: true;
+//   CampsiteLongitude: -118.0186111;
+//   CampsiteLatitude: 44.6969444;
+// };
 function App() {
-  const [parks, setParks] = useState<Parks[] | null>();
-  axios.defaults.baseURL =
-    'https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0';
-  axios.defaults.headers['apikey'] = '95150cf7-f71e-473a-8f4f-ff73f0d4766c';
-  axios.defaults.headers.post['Content-Type'] =
-    'application/x-www-form-urlencoded';
+  // const [parks, setParks] = useState<Parks[] | null>();
+  // axios.defaults.baseURL =
+  //   'https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0';
+  // axios.defaults.headers['apikey'] = '95150cf7-f71e-473a-8f4f-ff73f0d4766c';
+  // axios.defaults.headers.post['Content-Type'] =
+  //   'application/x-www-form-urlencoded';
 
-  useEffect(() => {
-    const url =
-      'https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0';
-    axios.get(url).then((response) => {
-      setParks(response.data);
-    });
-  });
+  // useEffect(() => {
+  //   const url =
+  //     'https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0';
+  //   axios.get(url).then((response) => {
+  //     setParks(response.data);
+  //   });
+  // });
 
   return (
     <div className="container">
@@ -35,11 +35,11 @@ function App() {
         <img src={logo} className="larkenCamp-logo" alt="logo" />
       </header>
       <div className="data">
-        {parks
+        {/* {parks
           ? parks.map((park) => {
               return <p>{park.CampsiteName + park.TypeOfUse}</p>;
             })
-          : null}
+          : null} */}
       </div>
     </div>
   );
