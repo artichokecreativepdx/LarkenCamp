6606e920f4901ec1b5416c606e8f805faf9ab523
megan <me.maldy@gmail.com>
2023-02-11 15:10:24 -0800

    created routes

---
+++ src/pages/page2.tsx
@@ -0,0 +1,5 @@
+const Page2: React.FC = (): JSX.Element => {
+  return <>2</>;
+};
+
+export default Page2;
---
+++ src/pages/page3.tsx
@@ -0,0 +1,5 @@
+const Page3: React.FC = (): JSX.Element => {
+  return <>3</>;
+};
+
+export default Page3;
